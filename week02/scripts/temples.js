fe6d4eedd83cf62dc34742990ccaa6ab3f608d2b
angelasmilford <angela.milford123@gmail.com>
2024-09-28 08:01:57 -1100

    for the Picture album activity

---
+++ week02/scripts/temples.js
@@ -0,0 +1,21 @@
+// Function to display the current year
+function displayCurrentYear() {
+    const currentYear = new Date().getFullYear(); // Get the current year
+    document.getElementById('currentyear').textContent = currentYear; // Update the year in the footer
+}
+
+// Function to display the last modified date
+function displayLastModified() {
+    const lastModifiedDate = document.lastModified; // Get the last modified date of the document
+    document.getElementById('lastModified').textContent = lastModifiedDate; // Update last modified in the footer
+}
+
+// Call the functions to update the footer
+displayCurrentYear();
+displayLastModified();
+
+//Hamburger effect that only shows in the mobile view
+
+//clicking the hamburger button toggles the navigation menu items from viewable to not viewable
+
+//use a symbol, such as 'X' to close the hamburger menu
