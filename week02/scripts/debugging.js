2cef2217a4d681e3cf6978a946e381d399108afa
angelasmilford <angela.milford123@gmail.com>
2024-09-27 20:34:42 -1100

    finished the debugging activity

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,6 +1,15 @@
 const PI = 3.14;
-const radius = 3;
+let radius = 3; //changed from const to let to allow value to change or be reassigned
 let area = 0;
-area = radius * radius * pi;
+function circleArea(radius){
+    const area = radius * PI;
+}
+area = circleArea(3);
+console.log(area);
+
+area = radius * radius * PI; //'pi' isn't defined because it isn't an established variable
+console.log("Area 1:", area);
+
 radius = 4;
-area = radius * radius * pi;
+area = radius * radius * PI;
+console.log("Area 2:", area);
