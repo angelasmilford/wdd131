08b9f2153c489826219d634f8e7ea0d2ac1997e1
angelasmilford <angela.milford123@gmail.com>
2024-10-04 00:21:53 -1100

    changes

---
+++ week03/scripts/place.js
@@ -0,0 +1,9 @@
+// Function to display the last modified date
+function displayLastModified() {
+    const lastModifiedDate = document.lastModified; // Get the last modified date of the document
+    document.getElementById('lastModified').textContent = lastModifiedDate; // Update last modified in the footer
+}
+
+// Call the functions to update the footer
+displayLastModified();
+
