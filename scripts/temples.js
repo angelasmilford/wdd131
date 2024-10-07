0132519d96cf59e5bc8712584d412ff992bb3c4d
angelasmilford <angela.milford123@gmail.com>
2024-10-06 21:45:15 -1100

    Changes for the hamburger. Now I'm craving a hamburger :'(

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,12 @@
+const hamButton = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
 // Function to display the current year
 function displayCurrentYear() {
     const currentYear = new Date().getFullYear(); // Get the current year
@@ -13,26 +22,3 @@ function displayLastModified() {
 // Call the functions to update the footer
 displayCurrentYear();
 displayLastModified();
-
-
-//Hamburger effect that only shows in the mobile view
-
-//clicking the hamburger button toggles the navigation menu items from viewable to not viewable
-
-//use a symbol, such as 'X' to close the hamburger menu
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => { //changed to 'click'
-	navigation.classList.toggle('open');//changed to 'classList' and 'open'
-	hamButton.classList.toggle('open');
-});
-
-
-const hamburger = document.querySelector('.hamburger');
-const header = document.querySelector('header');
-
-hamburger.addEventListener('click', () => {
-    header.classList.toggle('menu-active')
-});
