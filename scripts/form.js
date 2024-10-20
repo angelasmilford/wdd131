c69ff41837bd20088bd584823580605d4652d601
angelasmilford <angela.milford123@gmail.com>
2024-10-19 17:08:11 -1100

    form

---
+++ scripts/form.js
@@ -0,0 +1,45 @@
+// Function to display the current year
+function displayCurrentYear() {
+    const currentYear = new Date().getFullYear(); // Get the current year
+    document.getElementById('currentyear').textContent = currentYear; // Update the year in the footer
+}
+
+// Function to display the last modified date
+function displayLastModified() {
+    const lastModifiedDate = document.lastModified; // Get the last modified date of the document
+    document.getElementById('lastModified').textContent = lastModifiedDate; // Update last modified in the footer
+}
+
+// Call the functions to update the footer
+displayCurrentYear();
+displayLastModified();
+
+
+
+
+const products = [
+    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+  ];
+
+  // Populate Product Select Options
+  const productSelect = document.getElementById('product');
+  products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+});
+
+
+
+
+const reviewCount = localStorage.getItem('reviewCount') || 0;
+const newCount = parseInt(reviewCount) + 1;
+localStorage.setItem('reviewCount', newCount);
+
+const counterDiv = document.getElementById('review-counter');
+counterDiv.textContent = `You have submitted ${newCount} reviews.`;
